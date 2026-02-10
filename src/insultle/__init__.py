647d1ee073261ceb894d5bb5033b9a895e9f6d78
qqiann <zhangqianqian80@gmail.com>
2026-02-10 18:55:19 +0100

    Update __init__.py

--- src/insultle/__init__.py
+++ src/insultle/__init__.py
@@ -1,2 +1,34 @@
 def main() -> None:
     print("Hello from insultle!")
+
+import pygame
+
+pygame.init()
+
+Larghezza_Schermo = 822
+Altezza_Schermo = 745
+
+schermo = pygame.display.set_mode((Larghezza_Schermo, Altezza_Schermo)) 
+pygame.display.set_caption("Insultle") 
+
+#imgSfondo = pygame.image.load("sfondoINSULTLE.png") devo aggiungre il file dopo
+imgSfondo = pygame.transform.scale(imgSfondo,(SCREEN_WIDTH,SCREEN_HEIGHT))
+
+running = True
+#LetteraA = False
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
+                
+#         if event.type == pygame.KEYDOWN:
+#             if event.key == pygame.K_a:
+#                 LetteraA = True 
+                
+
+pygame.quit()
